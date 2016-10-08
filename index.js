81b0a3679eeacaaa5d6f26e54fc9446c39236e48
NekOrz <joe1256joe@gmail.com>
2016-10-08 08:58:18 -0700

    Finish HW2
    1. Add Function: curringSum
    
    2. Everything is done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let Counter = function f() {
+const Counter = function f() {
   let count = 0;
   return {
     getCount() {
@@ -12,6 +12,16 @@ let Counter = function f() {
     },
   };
 };
+
+const myCounter = new Counter();
+
+myCounter.getCount();
+myCounter.increase();
+myCounter.getCount();
+myCounter.decrease();
+myCounter.getCount();
+myCounter.decrease();
+myCounter.getCount();
 
 function getType(v) {
   const type = typeof v;
@@ -32,3 +42,26 @@ function getType(v) {
   }
   return type;
 }
+
+getType(1); // 'number'
+getType(NaN); // 'NaN'
+getType('1'); // 'string'
+getType(a => a); // 'function'
+getType({}); // 'object'
+getType([]); // 'array'
+getType(null); // 'null'
+getType(undefined); // 'undefined'
+
+function curringSum(value) { // WTF is this shit...BUT IT WORK! BLACK MAGIC.
+  const sum = value;
+  return function curringSum2(value2) {
+    const sum2 = sum + value2;
+    return function f2(value3) {
+      return sum2 + value3;
+    };
+  };
+}
+
+curringSum(1)(2)(3); // 6
+const add125 = curringSum(100)(25);
+add125(10); // 135
